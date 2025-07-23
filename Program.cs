1a591885c34d46823d6800c1f61f7adf9c5e251f
Orlando Santos <97381390+orlandonss@users.noreply.github.com>
2025-07-23 15:29:36 +0100

    local-server

--- Program.cs
+++ Program.cs
@@ -1,2 +1,63 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Hl7.Fhir.Model;
+using Hl7.Fhir.Rest;
+using System;
+
+namespace Project01
+{
+    public static class FhirProject
+    {
+        private static readonly Dictionary<string, string> fhir_Servers = new Dictionary<string, string>()
+        {
+            {"PublicHapi","http://hapi.fhir.org/baseDstu3"},
+            {"Local","http://localhost:8080/fhir"},
+        };
+        private static readonly string fhirServer = fhir_Servers["PublicHapi"];
+        static void Main(string[] args)
+        {
+            FhirClient client = new FhirClient(fhirServer)
+            {
+                Settings =
+                {
+                    PreferredFormat = ResourceFormat.Json,
+                    PreferredReturn = Prefer.ReturnRepresentation
+                }
+            };
+            var parameters = new string[] { "_summary=count" };
+            //condition to search for patients with the name test
+            var parameters2 = new string[] { "name=test" };
+            Bundle patientBundle = client.Search<Patient>(parameters);
+            Bundle patientBundleE = client.Search<Patient>(null);
+            Bundle patientBundleS = client.Search<Patient>(parameters2);
+
+
+            Console.WriteLine($"Total:{patientBundle.Total}");
+            Console.WriteLine($"Entry:{patientBundleE.Entry.Count}\n");
+
+            int entrycount = 1;
+            bool foundPatients = false;
+
+            while (patientBundleS != null && !foundPatients)
+            {
+                foreach (Bundle.EntryComponent entry in patientBundleS.Entry)
+                {
+                    if (entry.Resource is Patient patient)
+                    {
+                        foundPatients = true; // break outer loop after this batch
+
+                        Console.WriteLine($"Entry: {entrycount}\nID: {patient.Id}");
+
+                        if (patient.Name.Count > 0)
+                        {
+                            Console.WriteLine($"Name: {patient.Name[0]}\nUrl: {entry.FullUrl}");
+                        }
+
+                        var encounterBundle = client.Search<Encounter>(new string[] { $"patient=Patient/{patient.Id}" });
+                        Console.WriteLine($"EncountersTotal: {encounterBundle.Total}\n");
+                        entrycount++;
+                    }
+                }
+                patientBundleS = client.Continue(patientBundleS);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,30 @@
+# SETTUP AND CREATING AN API CLIENT-SERVER HL7-FHIR STU3 v3.0.1 IN WINDOWS Part2
+For this second part of the project the characterics of FHIR to be explored will be ADDING,Deleting and UPDATING 
+a patient in a local server.
+
+# NEEDED TOOLS
+For developing this part of the project it will be needed the same software as the previsously project present in the 
+repository: https://github.com/orlandonss/HL7-FHIR/tree/main/Projeto01.
+Also, it will be needed a docker to run a local server and perform some operations.
+
+- [Docker](https://www.docker.com)
+
+# SETUP A LOCAL SERVER IN HL7-FHIR
+For running a local server it will be needed to setup and configurate the docker.
+
+First you need to have the docker already installed in your local computer, then it is needed to run certain commands to start the docker and configurate him  to run the fhir application.
+
+
+
+
+# OPERATIONS IN HL7-FHIR PATIENT DATA
+
+
+## ADD PATIENT
+
+
+## UPDATE PATIENT
+
+
+
+## DELETE PATIENT
